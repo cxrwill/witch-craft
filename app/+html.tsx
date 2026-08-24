afbd52722161614fa11aca221bea0b144ae191e9
chenxiangru <cxrwill@users.noreply.github.com>
2026-08-25 00:51:33 +0800

    chore: FINAL BUILD 20250825

--- app/+html.tsx
+++ app/+html.tsx
@@ -8,7 +8,7 @@ export default function Root({ children }: PropsWithChildren) {
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <title>WitchCraft - 赛博女巫日记 v3 - 新增功能</title>
+        <title>WitchCraft 2.0 FINAL BUILD 20250825</title>
         <ScrollViewStyleReset />
         <script dangerouslySetInnerHTML={{
           __html: `
