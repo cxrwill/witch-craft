201f43a0478f97a8f2e15b55bc6efc30c1ff2ec6
chenxiangru <cxrwill@users.noreply.github.com>
2026-09-07 20:23:25 +0800

    fix: add base tag for GitHub Pages

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,35 +9,8 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <title>WitchCraft - 赛博女巫日记</title>
+        <base href="/witch-craft/" />
         <ScrollViewStyleReset />
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            (function() {
-              // Fix base URL for GitHub Pages subdirectory
-              var base = '/witch-craft';
-              var path = window.location.pathname;
-              var search = window.location.search;
-
-              // If on GitHub Pages subdirectory, fix the base path
-              if (path.indexOf(base) === 0) {
-                // Remove base prefix for router
-                var relativePath = path.substring(base.length);
-                if (!relativePath.startsWith('/')) {
-                  relativePath = '/' + relativePath;
-                }
-                // Update document base
-                var baseEl = document.createElement('base');
-                baseEl.href = base + '/';
-                document.head.appendChild(baseEl);
-
-                // If on root, redirect to proper path
-                if (path === base || path === base + '/') {
-                  window.history.replaceState({}, '', base + '/');
-                }
-              }
-            })();
-          `
-        }} />
       </head>
       <body>{children}</body>
     </html>
